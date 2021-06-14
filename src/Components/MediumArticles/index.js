289b8a4ddf6114bb2ac1b1d5a4364deecb5c29cf
Alison Major <alison.major@gmail.com>
2021-06-14 11:23:14 -0400

    Improve content of article card

--- src/Components/MediumArticles/index.js
+++ src/Components/MediumArticles/index.js
@@ -1,50 +1,75 @@
 import React from 'react';
 
 const MediumArticles = (props) => {
-  const { title, link, thumbnail, content, pubDate } = props.articleData;
+  const { title, link, thumbnail, content, pubDate, categories } = props.articleData;
 
-  function article() {
-    const articleTitle = truncateText(cleanTitle(title), 0, 60);
-    const articleContent = truncateText(toText(content), 48, 300) + "...";
+  const article = () => {
+    const articleTitle = cleanTitle(title);
+    const articleContent = toDescription(content);
+    const articlePublisher = toPublisher(content);
     const articlePublicationDate = convertDate(pubDate);
+    const articleCategories = categories.toString().replaceAll(",", ", ");
 
     return (
       <div className="card">
         <img src={`${thumbnail}`} className="card-img-top" alt={title} />
         <div className="card-body">
           <h5 className="card-title">{articleTitle}</h5>
-          <p className="card-text">{articleContent}</p>
-          <p className="card-text"><em>{articlePublicationDate}</em></p>
-          <a target="_blank" rel="noreferrer" className="btn btn-primary" href={`${link}`}>
-            Read more...
+          <p className="card-text">
+            {articleContent}
+          </p>
+          <p className="card-text">
+            <small><em>{articlePublicationDate}</em></small>
+          </p>
+          <a target="_blank" rel="noreferrer" className="btn btn-dark" href={`${link}`}>
+            Read on {articlePublisher}
           </a>
+          <p className="card-text" style={{ marginTop: "20px" }}>
+            <small><em>Categories: {articleCategories}</em></small>
+          </p>
         </div>
       </div>
     )
 
   }
 
-  function cleanTitle(checkTitle) {
+  const cleanTitle = (checkTitle) => {
     checkTitle = checkTitle.replace("amp;", "");
     return checkTitle
   }
 
-  function truncateText(text, start, len) {
-    return text.length > len ? text.slice(start, len) : text;
+  const toPublisher = (mediumContent) => {
+    const regex = /Continue reading on (.+) »/gm;
+    let m;
+    let publisherName = "Medium";
+    while ((m = regex.exec(mediumContent)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+      
+      // The result can be accessed through the `m`-variable.
+      // m.forEach((match, groupIndex) => {
+      //   console.log(`Found match, group ${groupIndex}: ${match}`);
+      // });
+
+      publisherName = m[1];
+    }
+    return publisherName;
   }
 
-  function toText(block) {
+  const toDescription = (block) => {
     let tag = document.createElement('div');
     tag.innerHTML = block;
     block = tag.innerText;
-    return block
+    const description = block.trim().split('\n')[0]; 
+    return description;
   }
 
-  function convertDate(date) {
+  const convertDate = (date) => {
     let dateArray = date.slice(0, 10).split("-")
     let year = dateArray.shift();
     dateArray.push(year)
-    console.log(date)
     return `Published: ${dateArray.join("/")}`;
   }
 
